b54b4b75c2a5432ee9afd8f38e346747a1a4d11b
Thetan <83221369+Thetan-ILW@users.noreply.github.com>
2024-09-07 09:47:13 +0000

    Inputs modal refactor

--- views/modals/Inputs/ViewConfig.lua
+++ views/modals/Inputs/ViewConfig.lua
@@ -1,7 +1,6 @@
 local IViewConfig = require("osu_ui.views.IViewConfig")
 
 local ui = require("osu_ui.ui")
-local just = require("just")
 local flux = require("flux")
 local math_util = require("math_util")
 local BackButton = require("osu_ui.ui.BackButton")
@@ -17,9 +16,9 @@ local Layout = require("osu_ui.views.OsuLayout")
 ---@field heightTween table?
 local ViewConfig = IViewConfig + {}
 
----@type table<string, string>
+---@type {[string]: string}
 local text
----@type table<string, love.Font>
+---@type {[string]: love.Font}
 local font
 
 ---@type love.Image
@@ -31,39 +30,34 @@ local back_button
 
 ---@param game sphere.GameController
 ---@param assets osu.ui.OsuAssets
----@param modal osu.ui.Modal
+---@param modal osu.ui.InputsModal
 function ViewConfig:new(game, assets, modal)
 	text, font = assets.localization:get("inputsModal")
 	assert(text and font)
 	self.game = game
 	self.assets = assets
 	self.modal = modal
-	self.currentMode = self.modal.inputMode
-	self.inputModel = self.game.inputModel
-	self:updateKeys()
+	self.inputModel = modal.inputModel
 	self:createUI()
+	self:updateView()
 	arrow_img = assets.images.inputsArrow
 end
 
-local modes = {
-	"1key",
-	"2key",
-	"3key",
-	"4key",
-	"5key",
-	"6key",
-	"7key",
-	"7key1scratch",
-	"8key",
-	"9key",
-	"10key",
-	"12key",
-	"12key2scratch",
-	"14key",
-	"14key2scratch",
-	"16key",
-	"16key2scratch",
-}
+local key_width = 50
+local key_height = 50
+local key_spacing = 4
+
+function ViewConfig:updateView()
+	self.bindFocus = { row = 1, key = 1 }
+
+	self.arrowPosition = 1
+	if self.arrowTween then
+		self.arrowTween:stop()
+	end
+
+	local rows = math.min(self.inputModel:getBindsCount(self.modal.mode) + 1, 6)
+	self.visualHeight = rows * key_height
+end
 
 function ViewConfig:createUI()
 	local assets = self.assets
@@ -76,11 +70,12 @@ function ViewConfig:createUI()
 		pixelWidth = 300,
 		pixelHeight = 37,
 	}, function()
-		return self.currentMode, modes
+		return modal.mode, modal.modes
 	end, function(v)
-		self.currentMode = v
-		self.inputs = input_model:getInputs(v)
-		self:updateKeys()
+		modal.mode = v
+		modal.inputs = input_model:getInputs(v)
+		modal:updateKeys()
+		self:updateView()
 	end)
 
 	back_button = BackButton(assets, { w = 93, h = 90 }, function()
@@ -88,187 +83,53 @@ function ViewConfig:createUI()
 	end)
 end
 
-local key_width = 50
-local key_height = 50
-local key_spacing = 4
-
-local colors = {
+local key_colors = {
 	white = { 1, 1, 1 },
 	pink = { 0.99, 0.49, 1 },
 	yellow = { 1, 0.87, 0.24 },
 }
 
-local colors_tinted = {
+local key_colors_tinted = {
 	white = { 0.65, 0.65, 0.65 },
 	pink = { 0.69, 0.34, 0.7 },
 	yellow = { 0.71, 0.62, 0.17 },
 }
 
-local key_colors = {
-	"white",
-	"pink",
-	"yellow",
-}
-
-local predefined_colors = {
-	{
-		"yellow",
-	},
-	{
-		"white",
-		"pink",
-	},
-	{
-
-		"white",
-		"pink",
-		"white",
-	},
-	{
-		"white",
-		"pink",
-		"pink",
-		"white",
-	},
-	{
-		"white",
-		"pink",
-		"yellow",
-		"pink",
-		"white",
-	},
-	{
-		-- 6key
-	},
-	{
-		"white",
-		"pink",
-		"white",
-		"yellow",
-		"white",
-		"pink",
-		"white",
-	},
-}
-
-function ViewConfig:updateKeys()
-	self.inputs = self.inputModel:getInputs(self.currentMode)
-	self.bindFocus = { row = 1, key = 1 }
-
-	self.arrowPosition = 1
-	if self.arrowTween then
-		self.arrowTween:stop()
-	end
-
-	local rows = math.min(self.inputModel:getBindsCount(self.currentMode) + 1, 6)
-	self.visualHeight = rows * key_height
-
-	self.colors = {}
-	self.names = {}
-
-	local keys = 0
-	local scratches = 0
-
-	for _, v in ipairs(self.inputs) do
-		if v:find("key") then
-			keys = keys + 1
-		elseif v:find("scratch") then
-			scratches = scratches + 1
-		end
-	end
-
-	local struct = 0
-	local symetrical = keys % 2 == 0
-	if keys < 5 then
-		struct = (keys - 1) % 4 + 1
-	elseif keys % 5 == 0 then
-		struct = 5
-	elseif keys % 6 == 0 then
-		struct = 3
-	elseif keys % 7 == 0 then
-		struct = 7
-	else
-		struct = symetrical and 4 or 3
-	end
-
-	local repetitions = keys / struct
-
-	local key = 1
-	for r = 1, repetitions do
-		for i, color in ipairs(predefined_colors[struct]) do
-			table.insert(self.colors, color)
-			table.insert(self.names, ("%iK"):format(key))
-			key = key + 1
-		end
-	end
-
-	if not symetrical then
-		self.colors[math.ceil(keys / 2)] = "yellow"
-	end
-
-	for i = 1, scratches do
-		table.insert(self.colors, key_colors[3])
-		table.insert(self.names, ("%iS"):format(i))
-	end
-
-	if #self.colors ~= keys + scratches then
-		self.colors = {}
-		self.names = {}
-		for i = 1, keys do
-			table.insert(self.colors, key_colors[i % 2 + 1])
-			table.insert(self.names, ("%iK"):format(i))
-		end
-		for i = 1, scratches do
-			table.insert(self.colors, key_colors[3])
-			table.insert(self.names, ("%iS"):format(i))
-		end
-	end
-end
-
 local gfx = love.graphics
 
 function ViewConfig:keysBackground(rows)
-	for i, v in ipairs(self.colors) do
-		gfx.setColor(colors_tinted[v])
+	local colors = self.modal.colors
+	local names = self.modal.names
+
+	for i, v in ipairs(colors) do
+		gfx.setColor(key_colors_tinted[v])
 		gfx.rectangle("fill", 0, -key_height * rows, key_width, key_height * (rows + 1), 4, 4)
 		gfx.rectangle("line", 0, -key_height * rows, key_width, key_height * (rows + 1), 4, 4)
-		gfx.setColor(colors[v])
+		gfx.setColor(key_colors[v])
 		gfx.rectangle("fill", 0, 0, key_width, key_height, 4, 4)
 		gfx.rectangle("line", 0, 0, key_width, key_height, 4, 4)
 		gfx.setColor(0, 0, 0)
-		ui.frame(self.names[i], 0, 0, key_width, key_height, "center", "center")
+		ui.frame(names[i], 0, 0, key_width, key_height, "center", "center")
 		gfx.translate(key_width + key_spacing, 0)
 	end
-end
-
-local function getKey()
-	local changed = false
-	local key, device, device_id
-	local k, dev, dev_id = just.next_input("pressed")
-	if just.keypressed("escape", true) then
-		return false
-	end
-	if k then
-		key, device, device_id = k, dev, dev_id
-		changed = true
-		just.reset()
-	end
-
-	return changed, key, device, device_id
 end
 
 ---@param input_key string
 ---@param input_index number
 ---@param row number
 function ViewConfig:buttons(input_key, input_index, row)
+	local modal = self.modal
 	local input_model = self.inputModel
-	local bind = input_model:getKey(self.currentMode, input_key, row)
+	local mode = modal.mode
+	local inputs = modal.inputs
+	local bind = input_model:getKey(mode, input_key, row)
+	local getKey = modal.getKey
 
 	if self.bindFocus.key == input_index and self.bindFocus.row == row then
 		local changed, key, device, device_id = getKey()
 
 		if changed then
-			input_model:setKey(self.currentMode, self.inputs[input_index], row, device, device_id, key)
+			input_model:setKey(mode, inputs[input_index], row, device, device_id, key)
 			self.bindFocus.key = input_index + 1
 
 			if self.arrowTween then
@@ -298,7 +159,7 @@ function ViewConfig:buttons(input_key, input_index, row)
 		end
 
 		if love.mouse.isDown(2) then
-			input_model:setKey(self.currentMode, self.inputs[input_index], row)
+			input_model:setKey(mode, inputs[input_index], row)
 		end
 
 		gfx.setColor(1, 1, 1, 0.3)
@@ -332,7 +193,7 @@ function ViewConfig:binds(rows, total_w)
 			gfx.line(-10, 0, total_w + 10, 0)
 		end
 
-		for i, v in ipairs(self.inputs) do
+		for i, v in ipairs(self.modal.inputs) do
 			self:buttons(v, i, row_i)
 		end
 		gfx.pop()
@@ -340,8 +201,9 @@ function ViewConfig:binds(rows, total_w)
 end
 
 function ViewConfig:keys(w, h)
-	local key_count = #self.colors
-	local rows = math.min(self.inputModel:getBindsCount(self.currentMode) + 1, 6)
+	local colors = self.modal.colors
+	local key_count = #colors
+	local rows = math.min(self.inputModel:getBindsCount(self.modal.mode) + 1, 6)
 
 	local total_w = key_width * key_count + (key_spacing * (key_count - 1))
 	gfx.setLineStyle("smooth")
@@ -377,7 +239,7 @@ function ViewConfig:resolutionUpdated()
 	self:createUI()
 end
 
-function ViewConfig:draw(modal)
+function ViewConfig:draw()
 	local w, h = Layout:move("base")
 
 	gfx.setColor(1, 1, 1, 1)
@@ -390,7 +252,7 @@ function ViewConfig:draw(modal)
 	gfx.pop()
 
 	gfx.push()
-	gfx.translate(540, 128)
+	gfx.translate(w / 2 - modes_combo:getWidth() / 2, 128)
 	modes_combo:update(true)
 	modes_combo:drawBody()
 	gfx.pop()
