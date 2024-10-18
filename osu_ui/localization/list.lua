43f312dfb2fca627604bf17c572e847b7bf82f4d
Thetan <thetan1337@gmail.com>
2024-10-18 11:23:23 +0000

    Add Spanish Translation, by Leoncheto and Sanik

--- osu_ui/localization/list.lua
+++ osu_ui/localization/list.lua
@@ -2,4 +2,5 @@ return {
 	{ name = "English", filepath = "en.lua" },
 	{ name = "English (Original)", filepath = "en_original.lua" },
 	{ name = "Русский", filepath = "ru.lua" },
+	{ name = "Español", filepath = "sp.lua" }
 }
